22f944aef68f93c842fb60f99e13f425d96c2629
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2022-10-10 13:07:36 -0500

    added coreutils

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,3 +1,4 @@
 path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/.dotfiles/bin"
-path_prepend "/opt/homebrew/bin"
+path_append "/opt/homebrew/bin"
+path_append "/opt/homebrew/opt/coreutils/libexec/gnubin"
